f8cfbc29a7f73aac203c5d09da72f3578569ba61
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-01-05 20:19:06 +0100

    added connection to WalletConnect Wallet

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
+import dev.pinkroom.walletconnectkit.WalletConnectButton
+import dev.pinkroom.walletconnectkit.WalletConnectKit
+import dev.pinkroom.walletconnectkit.WalletConnectKitConfig
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listOfIconUrlStrings = listOf<String>()
+        val config = WalletConnectKitConfig(
+            context = this,
+            relayUrl = "wss://bridge.aktionariat.com:8887",
+            appUrl = "walletconnectkit.com",
+            appName = "WalletConnectKit",
+            appDescription = "WalletConnectKit is the Swiss Army toolkit for WalletConnect!"
+        )
+
+        val walletConnectKit = WalletConnectKit.Builder(config).build()
+        val walletConnectButton: WalletConnectButton =  findViewById(R.id.bttnWalletConnect)
+
+        walletConnectButton.start(walletConnectKit) { address ->
+            println("You are connected with account: $address")
+        }
+
+
     }
 }
